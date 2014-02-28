4fa5988e204e1254364a4321bafa89455716df91
chupym <gabriel.nistor@sourcefabric.org>
2014-02-28 14:51:24 +0200

    Test for pip install

--- setup.py
+++ setup.py
@@ -16,14 +16,14 @@ setup(platforms=['all'],
       author_email='gabriel.nistor@sourcefabric.org',
       classifiers=['Development Status :: 4 - Beta'],
       description='The ally user management',
-      install_requires=['ally-core-http >= 1.0', 'ally-plugin >= 1.0', 'ally-hr-user >= 1.0', 'ally-security-user >= 1.0', 'ally-indexing-provider >= 1.0', 'ally-service-assemblage >= 1.0', 'ally-patch-praha'],
+      install_requires=['ally-core-http == 1.0', 'ally-plugin >= 1.0', 'ally-hr-user >= 1.0', 'ally-security-user >= 1.0', 'ally-indexing-provider >= 1.0', 'ally-service-assemblage >= 1.0', 'ally-patch-praha'],
       keywords=['Ally', 'user', 'management', 'service'],
       long_description='The components that represent the basic ally user management.',
       name='ally-user-management',
       packages=[],
       version='1.0',
       dependency_links=[
-        'git+https://github.com/sourcefabric/Ally-Py/tree/master/components#egg=ally-core-http'
+        'git+https://github.com/sourcefabric/Ally-Py/tree/master/components#egg=ally-core-http-1.0'
         ]
       )
 
