1f3988be81af35f2aa2e81c1b11c3f74bd9a80c9
chupym <gabriel.nistor@sourcefabric.org>
2014-02-28 15:33:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ from setuptools import setup
 
 # --------------------------------------------------------------------
 
+class my_install_scripts(install_scripts):
+    def run(self):
+        install_scripts.run(self)
+        
 setup(platforms=['all'],
       zip_safe=True,
       license='GPL v3',
@@ -16,14 +20,14 @@ setup(platforms=['all'],
       author_email='gabriel.nistor@sourcefabric.org',
       classifiers=['Development Status :: 4 - Beta'],
       description='The ally user management',
-      install_requires=['hr-user'],
+      install_requires=['ally_core_http'],
       keywords=['Ally', 'user', 'management', 'service'],
       long_description='The components that represent the basic ally user management.',
       name='ally-user-management',
       packages=[],
       version='1.0',
       dependency_links=[
-        'git+https://github.com/sourcefabric/Ally-Py/tree/master/components#egg=ally_core_http-1.0'
+        'https://github.com/sourcefabric/Ally-Py/tree/master/components#egg=ally_core_http'
         ]
       )
 
