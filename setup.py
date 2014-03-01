46c3b37826c4f8a0fa809575e20c44f239ccee64
chupym <gabriel.nistor@sourcefabric.org>
2014-03-02 01:06:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,16 @@ Created on Feb 28, 2014
 '''
 
 from setuptools import setup
+from setuptools.command.install import install
 
 # --------------------------------------------------------------------
 
+class CustomInstallCommand(install):
+    """Customized setuptools install command - prints a friendly greeting."""
+    def run(self):
+        print("================================================")
+        install.run(self)
+        
 setup(platforms=['all'],
       zip_safe=True,
       license='GPL v3',
@@ -16,14 +23,12 @@ setup(platforms=['all'],
       author_email='gabriel.nistor@sourcefabric.org',
       classifiers=['Development Status :: 4 - Beta'],
       description='The ally user management',
-      install_requires=['ally_core_http'],
       keywords=['Ally', 'user', 'management', 'service'],
       long_description='The components that represent the basic ally user management.',
       name='ally-user-management',
-      packages=[],
       version='1.0',
-      dependency_links=[
-        'https://github.com/sourcefabric/Ally-Py/tree/master/components#egg=ally_core_http'
-        ]
+    cmdclass={
+      'install': CustomInstallCommand,
+      },
       )
 
