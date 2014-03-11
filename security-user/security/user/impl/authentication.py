eba31481be444bb72b2f490613b8a008b111af9d
chupym <gabriel.nistor@sourcefabric.org>
2014-03-11 16:27:22 +0200

    Fix for forgottent test code.
    Fix for forgottent test code.

--- security-user/security/user/impl/authentication.py
+++ security-user/security/user/impl/authentication.py
@@ -102,21 +102,18 @@ class AuthenticationServiceAlchemy(SessionSupport, IAuthenticationService, IClea
         '''
         @see: IAuthenticationService.getGateways
         '''
-#         olderThan = self.session().query(current_timestamp()).scalar()
-#         olderThan -= self._sessionTimeOut
-#         sql = self.session().query(LoginMapped)
-#         sql = sql.filter(LoginMapped.Session == session)
-#         sql = sql.filter(LoginMapped.AccessedOn > olderThan)
-#         try: login = sql.one()
-#         except NoResultFound: raise InvalidError(_('Invalid session'), Login.Session)
-#         assert isinstance(login, LoginMapped), 'Invalid login %s' % login
-#         login.AccessedOn = current_timestamp()
-#         self.session().flush((login,))
-#         self.session().expunge(login)
-#         commitNow()
-        # TODO: Gabriel: remove and uncomment
-        login = Login()
-        login.User = int(session)
+        olderThan = self.session().query(current_timestamp()).scalar()
+        olderThan -= self._sessionTimeOut
+        sql = self.session().query(LoginMapped)
+        sql = sql.filter(LoginMapped.Session == session)
+        sql = sql.filter(LoginMapped.AccessedOn > olderThan)
+        try: login = sql.one()
+        except NoResultFound: raise InvalidError(_('Invalid session'), Login.Session)
+        assert isinstance(login, LoginMapped), 'Invalid login %s' % login
+        login.AccessedOn = current_timestamp()
+        self.session().flush((login,))
+        self.session().expunge(login)
+        commitNow()
         
         # We need to fore the commit because if there is an exception while processing the request we need to make
         # sure that the last access has been updated.
