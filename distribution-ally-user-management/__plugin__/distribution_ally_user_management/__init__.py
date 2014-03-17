2a4dbb0bd310ead4c078142f07f20178baae701c
chupym <gabriel.nistor@sourcefabric.org>
2014-03-17 11:57:25 +0200

    Patching gateway
    SD-65 : implement session expiration
    
    Patching gateway fix 2.

--- distribution-ally-user-management/__plugin__/distribution_ally_user_management/__init__.py
+++ distribution-ally-user-management/__plugin__/distribution_ally_user_management/__init__.py
@@ -21,5 +21,6 @@ LONG_DESCRIPTION = '''The components that represent the basic ally user manageme
 CLASSIFIERS = ['Development Status :: 4 - Beta']
 INSTALL_REQUIRES = ['ally-core-http >= 1.0', 'ally-plugin >= 1.0', 'ally-gui-core >= 1.0',
                     'ally-hr-user >= 1.0', 'ally-security-user >= 1.0', 'ally-indexing-provider >= 1.0',
-                    'ally-patch-praha', 'ally-patch-praha-gateway >= 1.0', 'ally-http-mongrel2-server >= 1.0', 
+                    'ally-patch-praha', 'ally-patch-praha-gateway >= 1.0', 'ally-http-mongrel2-server >= 1.0',
                     'ally-documentation >= 1.0', 'ally-service-assemblage >= 1.0']
+__extra__ = dict(package_data={'__setup__.distribution_ally_user_management': ['*.xml']})
